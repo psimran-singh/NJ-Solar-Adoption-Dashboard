04c5adc4dcd1131f041b29ab2aff7628ae75c7e0
Simran Singh <57107579+psimran-singh@users.noreply.github.com>
2022-04-10 22:39:48 -0400

    messing with sunroof conversions

---
+++ For Validation/Convert txt to csv.R
@@ -0,0 +1,2 @@
+zipcode_proj_nj <- read.csv("~/GitHub/Data-Visualization-Final-Project/For Validation/zipcode_proj_nj.txt")
+write_csv(zipcode_proj_nj,file="~/GitHub/Data-Visualization-Final-Project/For Validation/adoption_by_zip.csv")
