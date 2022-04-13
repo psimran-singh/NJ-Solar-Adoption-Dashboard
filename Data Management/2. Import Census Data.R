11218c59fe1960b9258cbba4297068ec0a304d43
YourName <your@mail.com>
2022-04-12 20:33:08 -0400

    Adding adoption rates to census data
    - adoption rate column

--- Data Management/2. Import Census Data.R
+++ Data Management/2. Import Census Data.R
@@ -12,12 +12,14 @@ county_dict <- c("1" = "Sussex","2"	= "Warren","3" = "Morris","4" = "Hunterdon",
 county_income <- get_acs(variables = "DP03_0051E", geography = "county", state = "NJ", survey = "acs5", year = 2019)
 county_perc_male <- get_acs(variables = "DP05_0002PE", geography = "county", state = "NJ", survey = "acs5", year = 2019)
 county_perc_white <- get_acs(variables = "DP05_0037PE", geography = "county", state = "NJ", survey = "acs5", year = 2019)
+county_total_house <- get_acs(variables = "DP04_0002E", geography = "county", state = "NJ", survey = "acs5", year = 2019)
 
 # add county code to county datasets
 county_dict <- county_dict[order(unlist(county_dict), decreasing=FALSE)] # sort dict by county name
 county_income["COUNTY_CODE"] = as.integer(names(county_dict))
 county_perc_male["COUNTY_CODE"] = as.integer(names(county_dict))
 county_perc_white["COUNTY_CODE"] = as.integer(names(county_dict))
+county_total_house["COUNTY_CODE"] = as.integer(names(county_dict))
 Solar_Res_County["COUNTY_CODE"] = as.integer(names(county_dict))
 
 # get zip data from ACS
@@ -29,14 +31,18 @@ zip_owner_house <- get_acs(variables = "DP04_0046E", geography = "zip code tabul
 zip_renter_house <- get_acs(variables = "DP04_0047E", geography = "zip code tabulation area", state = "NJ", survey = "acs5", year = 2019)
 
 # combine the county demographics with solar data
-Solar_Res_County$County_Income <- county_income$estimate[match(Solar_Res_County$COUNTY_CODE,county_income$COUNTY_CODE)]
-Solar_Res_County$County_Perc_White <- county_perc_white$estimate[match(Solar_Res_County$COUNTY_CODE,county_perc_white$COUNTY_CODE)]
-Solar_Res_County$County_Perc_Male <- county_perc_male$estimate[match(Solar_Res_County$COUNTY_CODE,county_perc_male$COUNTY_CODE)]
+Solar_Res_County$Income <- county_income$estimate[match(Solar_Res_County$COUNTY_CODE,county_income$COUNTY_CODE)]
+Solar_Res_County$Perc_White <- county_perc_white$estimate[match(Solar_Res_County$COUNTY_CODE,county_perc_white$COUNTY_CODE)]
+Solar_Res_County$Perc_Male <- county_perc_male$estimate[match(Solar_Res_County$COUNTY_CODE,county_perc_male$COUNTY_CODE)]
+Solar_Res_County$Total_Occ_House <- county_total_house$estimate[match(Solar_Res_County$COUNTY_CODE,county_total_house$COUNTY_CODE)]
+Solar_Res_County$Adoption_Rate <- Solar_Res_County$COUNT/Solar_Res_County$Total_Occ_House
 
 # combine the zip demographics with solar data
-Solar_Res_Zip$Zip_Income <- zip_income$estimate[match(Solar_Res_Zip$ZIP,zip_income$GEOID)]
-Solar_Res_Zip$Zip_Perc_White <- zip_perc_white$estimate[match(Solar_Res_Zip$ZIP,zip_perc_white$GEOID)]
-Solar_Res_Zip$Zip_Perc_Male <- zip_perc_male$estimate[match(Solar_Res_Zip$ZIP,zip_perc_male$GEOID)]
+Solar_Res_Zip$Income <- zip_income$estimate[match(Solar_Res_Zip$ZIP,zip_income$GEOID)]
+Solar_Res_Zip$Perc_White <- zip_perc_white$estimate[match(Solar_Res_Zip$ZIP,zip_perc_white$GEOID)]
+Solar_Res_Zip$Perc_Male <- zip_perc_male$estimate[match(Solar_Res_Zip$ZIP,zip_perc_male$GEOID)]
+Solar_Res_Zip$Total_Occ_House <- zip_total_house$estimate[match(Solar_Res_Zip$ZIP,zip_total_house$GEOID)]
+Solar_Res_Zip$Adoption_Rate <- Solar_Res_Zip$COUNT/Solar_Res_Zip$Total_Occ_House
 
 # combine zip housing data
 zip_total_house$Owner_Occ_Houses <- zip_owner_house$estimate[match(zip_total_house$GEOID,zip_owner_house$GEOID)]
@@ -46,7 +52,7 @@ zip_housing <- zip_total_house %>%
   subset(select = -c(NAME, variable, moe))
 
 # remove census datasets from global environment
-remove(zip_income, zip_perc_male, zip_perc_white, zip_total_house, zip_owner_house, zip_renter_house, county_dict, county_income, county_perc_male, county_perc_white)
+remove(zip_income, zip_perc_male, zip_perc_white, zip_total_house, zip_owner_house, zip_renter_house, county_dict, county_income, county_perc_male, county_perc_white, county_total_house)
 
 # final datasets
 setwd("~/Data Viz in R/Data-Visualization-Final-Project/Data Files for Analysis")
