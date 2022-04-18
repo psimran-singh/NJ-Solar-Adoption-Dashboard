fd2e4224e48aae525858ba546349fd3f15f89fa0
Simran Singh <57107579+psimran-singh@users.noreply.github.com>
2022-04-18 12:46:24 -0400

    ROLLED BACK TO OLDER VERSION, ADDED JESSICA'S SCRIPTS BACK IN

--- For Validation/Convert txt to csv.R
+++
@@ -1,2 +0,0 @@
-zipcode_proj_nj <- read.csv("~/GitHub/Data-Visualization-Final-Project/For Validation/zipcode_proj_nj.txt")
-write_csv(zipcode_proj_nj,file="~/GitHub/Data-Visualization-Final-Project/For Validation/adoption_by_zip.csv")
